d79139ab2e81369661740c347c2b71c8d9bae629
2kingmartians <legokingwalle@gmail.com>
2018-10-18 17:44:21 -0700

    Added changes to main menu
    Also added space station scene

---
+++ Astron End/Assets/Scenes/GravityTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTimer : MonoBehaviour
+{
+
+private Rigidbody rigid;
+[SerializeField]
+private Light lightSource;
+
+private float timer = 20f;
+
+void Start () {
+	rigid = GetComponent<Rigidbody>();
+}
+
+// Update is called once per frame
+void Update () {
+	timer -= Time.deltaTime;
+	if (timer < 0) {
+		rigid.useGravity = !rigid.useGravity;
+		if (rigid.useGravity)
+		{
+			lightSource.color = Color.green;
+		}
+		else {
+			lightSource.color = Color.red;
+		}
+
+		timer = 15f;
+	}
+}
+}
